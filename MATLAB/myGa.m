ce7d351d336a4b68645814089aaba3d5a2125a25
WhiteGrayxp <45351390+WhiteGrayxp@users.noreply.github.com>
2018-12-04 23:46:49 +0800

    添加适应度绘图

--- MATLAB/myGa.m
+++ MATLAB/myGa.m
@@ -3,4 +3,5 @@ clf;
 thres = 5;
 lb = [0;0];
 ub = [1/6;1/6];
-[X,FVAL,EXITFLAG,OUTPUT] = ga(@myfit,2,[],[],[],[],lb,ub);
+options = optimoptions('ga','PlotFcn', @gaplotbestf);
+[X,FVAL,EXITFLAG,OUTPUT] = ga(@myfit,2,[],[],[],[],lb,ub,[],[],options);
