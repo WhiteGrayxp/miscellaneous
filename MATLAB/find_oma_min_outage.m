8232eba196bba8614fd03fd82047868bebe30572
WhiteGrayxp <xpgray@foxmail.com>
2018-12-20 15:48:41 +0800

    添加OMA对照

---
+++ MATLAB/find_oma_min_outage.m
@@ -0,0 +1,15 @@
+function [outage,position] = find_oma_min_outage(sigma,d1,d2,a,thres)
+% sigma = 1/10^6;
+% d1 = 15;
+% d2 = 30;
+% a = 3;
+% thres = 5;
+fun = @(x)4-exp(-d1^a*thres*sigma/x(1))-exp(-d2^a*thres*sigma/x(2))-exp(-d1^a*thres*sigma/x(3))-exp(-d2^a*thres*sigma/x(3));
+x0 = [1,1,1];
+Aeq = [1,1,1];
+beq = 2;
+% p = [1,1,1]
+% y = 4-exp(-d1^a*thres*sigma/p(1))-exp(-d2^a*thres*sigma/p(2))-exp(-d1^a*thres*sigma/p(3))-exp(-d2^a*thres*sigma/p(3));
+[x,fval] = fmincon(fun,x0,[],[],Aeq,beq);
+outage = fval;
+position = x;
