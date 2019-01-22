86af0a01891d639dfb85f6125b2a928ba52b2713
WhiteGrayxp <xpgray@foxmail.com>
2019-01-22 16:06:09 +0800

    。

--- MATLAB/minmax/main.m
+++ MATLAB/minmax/main.m
@@ -9,13 +9,21 @@ thres = 5;
 x_axis = zeros(8,1);
 out_noma_minmax = zeros(8,1);
 out_oma_minmax = zeros(8,1);
+p3 = zeros(8,1);
+p4 = zeros(8,1);
+
 for loop = 1:8
     SIR = loop*5+50;
     x_axis(loop) = SIR;
     sigma = 10^(-SIR/10);
-    [outage,~] = find_noma_min_max(sigma,d1,d2,a,thres);
+    [outage,position] = find_noma_min_max(sigma,d1,d2,a,thres);
     out_noma_minmax(loop) = outage;
     out_oma_minmax(loop) = 1 - exp(-2*d2^a*sigma*thres/3);
+    
+    p3(loop) = position(1);
+    p4(loop) = position(2);
+    fprintf('p3:%.3f\n',position(1));
+    fprintf('p4:%.3f',position(2));
 end
 semilogy(x_axis,out_noma_minmax,'b-*','LineWidth',2,'MarkerSize',10);hold on;
 semilogy(x_axis,out_oma_minmax,'r-*','LineWidth',2,'MarkerSize',10);
